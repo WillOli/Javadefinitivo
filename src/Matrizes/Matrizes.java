afc20adfd29e660e7eff22d430dba2dc0ee61022
WillOli <william.ika@gmail.com>
2023-05-25 21:26:43 -0300

    Matrizes na pasta

--- src/Matrizes/Matrizes.java
+++ src/Matrizes/Matrizes.java
@@ -1,3 +1,5 @@
+package Matrizes;
+
 import java.util.Scanner;
 
 public class Matrizes {
@@ -24,7 +26,7 @@ public class Matrizes {
         System.out.println("\nMATRIZ DIGITADA: ");
         for (i = 0; i < M; i++) {
             for (j = 0; j < N; j++) {
-                System.out.println(mat[i][j] + " ");
+                System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
