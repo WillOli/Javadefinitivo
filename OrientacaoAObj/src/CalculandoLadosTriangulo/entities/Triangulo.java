7084bda904757b94e3a2a8c107462ea2a2dacfc2
WillOli <william.ika@gmail.com>
2023-06-01 19:51:24 -0300

    Adicionando método à classe

--- OrientacaoAObj/src/CalculandoLadosTriangulo/entities/Triangulo.java
+++ OrientacaoAObj/src/CalculandoLadosTriangulo/entities/Triangulo.java
@@ -7,4 +7,12 @@ public class Triangulo {
     public double b;
     public double c;
 
+
+    /* Chamando os atributos da class.
+    Função(método) sem parâmetros, retornando o calculo da área do trinâgulo.*/
+    public double   area() {
+        double p = (a + b + c ) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
 }
