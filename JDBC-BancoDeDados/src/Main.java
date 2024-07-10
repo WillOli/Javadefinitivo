eca6b098afbd950d461052203a0c6498304a900f
WillOli <william.ika@gmail.com>
2024-07-10 15:14:53 -0300

    feat: implementar inserção de vendedor no banco de dados

--- JDBC-BancoDeDados/src/Main.java
+++ JDBC-BancoDeDados/src/Main.java
@@ -1,9 +1,8 @@
 import DB.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,26 +11,67 @@ public class Main {
 //        DB.closeConnetion();
 //        System.out.println("Conexão encerrada!" );
 
+//        Connection conn = null;
+//        Statement st = null;
+//        ResultSet rs = null;
+//
+//        try {
+//            conn = DB.getConnection();
+//            st = conn.createStatement();
+//            rs = st.executeQuery("select * from department");
+//
+//            while (rs.next()) {
+//                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+//            }
+//        }
+//        catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            DB.closeResultSet(rs);
+//            DB.closeStatement(st);
+//            DB.closeConnection();
+//        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
+           try {
+                conn = DB.getConnection();
 
-        try {
-            conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
+               st = conn.prepareStatement(
+                       "INSERT INTO seller "
+                               + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                               + "VALUES "
+                               + "(?, ?, ?, ?, ?)",
+                       Statement.RETURN_GENERATED_KEYS);
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DB.closeResultSet(rs);
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+               st.setString(1, "Carl Purple");
+               st.setString(2, "carl@gmail.com");
+               st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+               st.setDouble(4, 3000.0);
+               st.setInt(5, 4);
+
+               int rowsAffected = st.executeUpdate();
+
+               System.out.println("Done! Rows affected " + rowsAffected);
+
+           }
+           catch (SQLException e) {
+               e.printStackTrace();
+           }
+           catch (ParseException e) {
+               e.printStackTrace();
+           }
+           finally {
+               DB.closeStatement(st);
+               DB.closeConnection();
+           }
+
+
+
+
+
+
+
     }
 }
